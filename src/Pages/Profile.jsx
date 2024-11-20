d44f99bb3af0c10982b7db734ba68e0f3ba3fdd2
ayan-akd <ayankumar.akd@gmail.com>
2024-11-20 23:20:16 +0600

    added loading

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -5,21 +5,28 @@
 import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import ProfileCard from "../Component/ProfileCard";
+import Loading from "../Component/Loading";
 
 const Profile = () => {
-  const { userData } = useContext(AuthContext);
- 
+  const { userData, roleLoading } = useContext(AuthContext);
+
   return (
-    <div className="max-w-screen-xl mx-auto p-10">
+    <div>
+      {roleLoading ? (
+        <Loading></Loading>
+      ) : (
+        <div className="max-w-screen-xl mx-auto p-10">
           <ProfileCard userData={userData}></ProfileCard>
           <div>
-          <h1 className="text-3xl md:text-5xl text-center pt-16 my-12">
-            Purchase <span className="text-grn">History</span>
-          </h1>
-          <h1 className="text-center font-bold text-3xl">
-                No History Found!!!
-              </h1>
+            <h1 className="text-3xl md:text-5xl text-center pt-16 my-12">
+              Purchase <span className="text-grn">History</span>
+            </h1>
+            <h1 className="text-center font-bold text-3xl">
+              No History Found!!!
+            </h1>
           </div>
+        </div>
+      )}
     </div>
   );
 };
