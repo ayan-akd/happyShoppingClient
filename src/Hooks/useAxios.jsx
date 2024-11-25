9b3bb255dfa10b83d7286da9883cb43aaca872ac
PallabKumarS <pallabkumar26@gmail.com>
2024-11-26 03:20:00 +0600

    changed url

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const baseURL = "https://happy-shopping-server.vercel.app";
-export const baseURL = "http://localhost:5000";
+export const baseURL = "https://happy-shopping-server.vercel.app";
+// export const baseURL = "http://localhost:5000";
 
 const axiosSecure = axios.create({
   baseURL: baseURL,
